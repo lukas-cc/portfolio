a07824513b901c1f2dfb07ab223fed769cedb657
lukasc <lukas.ciepielowski@gmx.de>
2022-10-29 17:07:41 +0200

    better transtion

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,11 @@ const Home: NextPage = () => {
   }, [control, inView]);
 
   const boxVariant = {
-    visible: { opacity: 1, scale: 1, transition: { duration: 1 } },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: { duration: 1, ease: "easeOut" },
+    },
     hidden: { opacity: 0, scale: 0 },
   };
 
