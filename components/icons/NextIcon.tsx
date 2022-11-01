48720e13556345f4ae75387ff40968b74078dee4
Lukas Ciepielowski <lukas.ciepielowski@dhcp-172-21-67-155.wlan.uni-hamburg.de.local>
2022-11-01 11:46:47 +0100

    change icon sizes

--- components/icons/NextIcon.tsx
+++ components/icons/NextIcon.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export const NextIcon = () => {
   return (
     <svg
-      width="394"
-      height="80"
+      width="160"
+      height="120"
       viewBox="0 0 394 80"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- components/icons/ReactIcon.tsx
+++ components/icons/ReactIcon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ReactIcon = () => {
   return (
-    <svg viewBox="-11.5 -10.23174 23 20.46348">
+    <svg viewBox="-11.5 -10.23174 23 20.46348" height="64" width="64">
       <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
       <g stroke="#61dafb" strokeWidth="1" fill="none">
         <ellipse rx="11" ry="4.2" />
