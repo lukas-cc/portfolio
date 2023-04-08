4e926d572395d67fcf531eedb5401ca122ef9407
lukas-cc <lukas.ciepielowski@gmx.de>
2023-04-08 22:37:17 +0200

    replace @next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "../../styles/globals.css";
-import { Quicksand } from "@next/font/google";
+import { Quicksand } from "next/font/google";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 export const metadata = {
