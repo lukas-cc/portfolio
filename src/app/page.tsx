21f0fce1dd8fe1cf75dfffe312853e3b7b8fd7ad
lukas-cc <lukas.ciepielowski@gmx.de>
2024-04-09 00:56:33 +0200

    update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,15 @@ const Home = () => {
   return (
     <main className="flex flex-col items-center justify-center gap-4 p-6 pt-20 md:p-8">
       <TypingAnimation />
-      <p className="text-center text-2xl">
-        A frontend software engineer. Doing my masters in computer science at
-        University of Hamburg.
-      </p>
-      <p className="text-center text-2xl">
-        Currently working as a working student at E&V Technology.
-      </p>
+      <section className="flex w-full flex-col gap-4 md:w-2/3">
+        <p className="text-center text-2xl">
+          A frontend software engineer. Doing my masters in computer science at
+          the University of Hamburg.
+        </p>
+        <p className="text-center text-2xl">
+          Currently working at E&V Technology.
+        </p>
+      </section>
       <div className="flex flex-col gap-20 pb-20 pt-20 md:pb-4">
         <SkillSection />
         <ProjectGrid />
