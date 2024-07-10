2677129bb42175513f1c746da507b94267b932a1
lukas-cc <lukas.ciepielowski@gmx.de>
2024-07-10 23:25:01 +0200

    upgrade packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import "../../styles/globals.css";
 import { Quicksand } from "next/font/google";
 import { Header } from "../components/header";
 import { Footer } from "@/components/footer";
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 
@@ -26,6 +26,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
