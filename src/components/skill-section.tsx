6bee287d5e384f7d1af9bd0bf506e79f28d4422d
Ciepielowski <lukasciepielowski@gmail.com>
2025-10-15 23:18:40 +0200

    remove wrapping div

--- src/components/skill-section.tsx
+++ src/components/skill-section.tsx
@@ -23,41 +23,28 @@ export const SkillSection = () => {
   return (
     <section className="p-8">
       <p className="pb-4 text-center text-2xl">My tech stack:</p>
-      <div className="grid auto-rows-fr grid-cols-1 gap-10 pt-16 md:grid-cols-2 md:px-36 lg:grid-cols-3">
-        <div className="flex w-full justify-center">
-          <Card
-            icon={iconArray[0]}
-            text="For typesafety and better developer experience."
-          />
-        </div>
-        <div className="flex w-full justify-center">
-          <Card icon={iconArray[1]} text="My preferred frontend framework." />
-        </div>
-
-        <div className="flex w-full justify-center">
-          <Card
-            icon={iconArray[2]}
-            text="Fullstack web at its best - makes building fullstack apps a breeze."
-          />
-        </div>
-        <div className="flex w-full justify-center">
-          <Card
-            icon={iconArray[3]}
-            text="Using one language for the whole stack."
-          />
-        </div>
-        <div className="flex w-full justify-center">
-          <Card
-            icon={iconArray[4]}
-            text="My go to styling solution, by far the most enjoyable."
-          />
-        </div>
-        <div className="flex w-full justify-center">
-          <Card
-            icon={iconArray[5]}
-            text="Ready-to-use components. Primarly used at work."
-          />
-        </div>
+      <div className="grid auto-rows-fr grid-cols-1 place-items-center gap-10 pt-16 md:grid-cols-2 md:px-36 lg:grid-cols-3">
+        <Card
+          icon={iconArray[0]}
+          text="For typesafety and better developer experience."
+        />
+        <Card icon={iconArray[1]} text="My preferred frontend framework." />
+        <Card
+          icon={iconArray[2]}
+          text="Fullstack web at its best - makes building fullstack apps a breeze."
+        />
+        <Card
+          icon={iconArray[3]}
+          text="Using one language for the whole stack."
+        />
+        <Card
+          icon={iconArray[4]}
+          text="My go to styling solution, by far the most enjoyable."
+        />
+        <Card
+          icon={iconArray[5]}
+          text="Ready-to-use components. Primarly used at work."
+        />
       </div>
     </section>
   );
