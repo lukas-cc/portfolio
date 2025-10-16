34811b077c29dfae44afa0290e49ed5ddb4812f8
Ciepielowski <lukasciepielowski@gmail.com>
2025-10-16 11:07:14 +0200

    fixes

--- src/components/project-showcase/project-card.tsx
+++ src/components/project-showcase/project-card.tsx
@@ -22,29 +22,28 @@ export const ProjectCard = ({
   href,
 }: ProjectCardProps) => {
   return (
-    <div className="group relative">
-      <div className="bg-primary absolute -inset-0.5 min-w-80 rounded-xl opacity-50 blur-xs transition duration-1000 group-hover:opacity-75 group-hover:duration-200" />
-      <Card className="relative flex flex-col justify-between">
-        <CardContent className="p-1">{children}</CardContent>
-        <div>
-          <CardHeader>
-            <CardTitle>{title}</CardTitle>
-            <CardDescription>{text}</CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Button>
-              <a
-                className="btn-primary btn btn-sm"
-                href={href}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Demo
-              </a>
-            </Button>
-          </CardFooter>
-        </div>
-      </Card>
-    </div>
+    <Card className="relative flex flex-col justify-between border border-white/20">
+      <CardContent className="p-1">
+        <div className="relative min-h-[270px] w-full">{children}</div>
+      </CardContent>
+      <div>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{text}</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <Button>
+            <a
+              className="btn-primary btn btn-sm"
+              href={href}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Demo
+            </a>
+          </Button>
+        </CardFooter>
+      </div>
+    </Card>
   );
 };
