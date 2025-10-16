9c6717f9e6b47a75b2897e12b1f9fb9c1095f105
Ciepielowski <lukasciepielowski@gmail.com>
2025-10-16 10:48:58 +0200

    change width and height of image

--- src/components/project-showcase/project-grid.tsx
+++ src/components/project-showcase/project-grid.tsx
@@ -15,8 +15,8 @@ export const ProjectGrid = () => {
             src="/weather-app.png"
             alt="weather app"
             priority
-            width={370}
-            height={250}
+            width={420}
+            height={270}
             sizes="100vw"
             className="h-full w-full rounded-lg object-cover"
           />
@@ -30,8 +30,8 @@ export const ProjectGrid = () => {
             src="/spotify.png"
             alt="spotify"
             priority
-            width={370}
-            height={250}
+            width={420}
+            height={270}
             sizes="100vw"
             className="h-full w-full rounded-lg object-cover"
           />
@@ -45,8 +45,8 @@ export const ProjectGrid = () => {
             src="/stock-io.png"
             alt="stockio"
             priority
-            width={370}
-            height={250}
+            width={420}
+            height={270}
             sizes="100vw"
             className="h-full w-full rounded-lg object-cover"
           />
