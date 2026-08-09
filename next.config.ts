e6d2c7ac5265f070bab1261f8e8fe8fc34fa1b2b
lukas-cc <lukas.ciepielowski@gmx.de>
2026-08-09 20:08:37 +0200

    bump deps

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  cacheComponents: true,
+  partialPrefetching: true,
+  reactCompiler: true,
 };
 
 module.exports = nextConfig;
